d6b567c3bfc22cb01b0db6a41c4b31c9ec7dfd74
Onethity <vasstog.ya@yandex.ru>
2019-09-14 17:52:24 +0300

    type validation

--- app/Service/QueryValidator.php
+++ app/Service/QueryValidator.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Aviprotest\Service;
 
 use Aviprotest\Exception\ValidatorException;
+use Aviprotest\Entity\Storage;
 
 class QueryValidator
 {
@@ -19,6 +20,24 @@ class QueryValidator
         if($id < 1) {
             throw new ValidatorException('ID must be an integer value and can not be less than 1');
 
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Checks if type is valid
+     */
+    public function isTypeValid($type): bool
+    {
+        if(!in_array($type, [
+            Storage::TYPE_STRING, 
+            Storage::TYPE_INT, 
+            Storage::TYPE_GUID, 
+            Storage::TYPE_TEXT
+            ])) {
+            throw new ValidatorException('Incorrect type of random value');
+        
         } else {
             return true;
         }
