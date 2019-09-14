5cbfe95cd01bc85fdd1767e6f241c8c3d204caa9
Onethity <vasstog.ya@yandex.ru>
2019-09-14 17:34:07 +0300

    isValidId() is created

--- app/Service/QueryValidator.php
+++ app/Service/QueryValidator.php
@@ -3,9 +3,24 @@
  * It validates request query params such as id, length and so on...
  */
 
+declare(strict_types=1);
+
 namespace Aviprotest\Service;
+
+use Aviprotest\Exception\ValidatorException;
 
 class QueryValidator
 {
+    /**
+     * Checks if id value is valid
+     */
+    public function isIdValid($id): bool
+    {
+        if($id < 1) {
+            throw new ValidatorException('ID must be an integer value and can not be less than 1');
 
+        } else {
+            return true;
+        }
+    }
 }
