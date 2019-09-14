2c073da3a52956d307af9bc96a5453f78bfaa59c
Onethity <vasstog.ya@yandex.ru>
2019-09-14 11:01:03 +0300

    created

---
+++ app/Entity/Storage.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The class describes a storage entity
+ */
+
+namespace Aviprotest\Entity;
+
+class Storage 
+{
+    /** @var int id of entity */
+    public $id;
+
+    /** @var string value of entity */
+    public $value;
+}
