f1e7c187183cc00c3b6227c65cc37bb1d504741c
Onethity <vasstog.ya@yandex.ru>
2019-09-14 12:03:08 +0300

    create fillRandom()

--- app/Entity/Storage.php
+++ app/Entity/Storage.php
@@ -9,9 +9,21 @@ namespace Aviprotest\Entity;
 
 class Storage 
 {
-    /** @var int id of entity */
+    /** 
+     * @var int id of entity 
+     */
     public $id;
 
-    /** @var string value of entity */
+    /** 
+     * @var string value of entity
+     */
     public $value;
+
+    /**
+     * It fills an antity with random value
+     */
+    public function fillRandom()
+    {
+        $this->value = random_int(PHP_INT_MIN, PHP_INT_MAX);
+    }
 }
